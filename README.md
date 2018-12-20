3d322e8fd696a1396571924e2a22184d48e2fc5d
it-toppp <36742579+it-toppp@users.noreply.github.com>
2018-12-20 11:01:36 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+wget https://raw.githubusercontent.com/it-toppp/anbox/master/anbox.sh && chmod +x ./anbox.sh && ./anbox.sh
---
+++ anbox.sh
@@ -0,0 +1,28 @@
+sudo apt update
+sudo apt install xubuntu-desktop -y
+sudo add-apt-repository ppa:morphis/anbox-support -y
+sudo apt update
+sudo apt install xubuntu-desktop anbox-modules-dkms snap wget mc git htop lzip unzip squashfs-tools -y
+sudo modprobe ashmem_linux
+sudo modprobe binder_linux
+sudo snap install --devmode --beta anbox
+sudo apt install android-tools-adb -y
+
+useradd -G sudo -d /home/test -s /bin/bash test
+mkdir /home/test
+chown test:test -R /home/test
+
+cd /home/test
+wget https://www.cdn.whatsapp.net/android/2.18.379/WhatsApp.apk
+wget https://github.com/it-toppp/anbox/raw/master/AutoResponder.apk
+#adb install ./WhatsApp.apk
+#adb install ./AutoResponder.apk
+
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+chmod 777 ./teamviewer_amd64.deb
+apt install ./teamviewer_amd64.deb -y
+teamviewer passwd 90909090
+teamviewer info
+passwd test
+
+reboot
