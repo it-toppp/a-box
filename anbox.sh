ce1002b2b436cb654a4e3008e3b9c0b6f608d89e
it-toppp <36742579+it-toppp@users.noreply.github.com>
2018-12-21 17:15:19 +0300

    Update anbox.sh

--- anbox.sh
+++ anbox.sh
@@ -15,12 +15,13 @@ sudo snap install --devmode --beta anbox
 sudo apt install android-tools-adb -y
 
 cd /home/test
-#wget https://www.cdn.whatsapp.net/android/2.18.379/WhatsApp.apk
-#wget https://github.com/it-toppp/anbox/raw/master/AutoResponder.apk
+wget https://www.cdn.whatsapp.net/android/2.18.379/WhatsApp.apk
+wget https://github.com/it-toppp/anbox/raw/master/AutoResponder.apk
 #adb install ./WhatsApp.apk
 #adb install ./AutoResponder.apk
 wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 chmod 777 ./teamviewer_amd64.deb
 apt install ./teamviewer_amd64.deb -y
 teamviewer passwd $TIMPASS
+ping 8.8.8.8 c 10
 teamviewer info
