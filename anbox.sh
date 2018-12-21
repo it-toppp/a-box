58e8f1100da6f404d5df731133a202578bdbdccc
it-toppp <36742579+it-toppp@users.noreply.github.com>
2018-12-21 10:40:37 +0300

    Update anbox.sh

--- anbox.sh
+++ anbox.sh
@@ -1,3 +1,9 @@
+echo "Enter teamviewer password: "
+read TIMPASS
+
+useradd -G sudo -d /home/test -m -s /bin/bash test
+passwd test
+
 sudo apt update
 sudo apt install xubuntu-desktop -y
 sudo add-apt-repository ppa:morphis/anbox-support -y
@@ -8,17 +14,13 @@ sudo modprobe binder_linux
 sudo snap install --devmode --beta anbox
 sudo apt install android-tools-adb -y
 
-useradd -G sudo -d /home/test -m -s /bin/bash test
-
 cd /home/test
 #wget https://www.cdn.whatsapp.net/android/2.18.379/WhatsApp.apk
 #wget https://github.com/it-toppp/anbox/raw/master/AutoResponder.apk
 #adb install ./WhatsApp.apk
 #adb install ./AutoResponder.apk
-
 wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 chmod 777 ./teamviewer_amd64.deb
-dpkg -i ./teamviewer_amd64.deb -y
-teamviewer passwd 90909090
-passwd test
+apt install ./teamviewer_amd64.deb -y
+teamviewer passwd $TIMPASS
 teamviewer info
