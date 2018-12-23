2548e7ef7ec49a008c1895fc1e32e05be554ed56
it-toppp <36742579+it-toppp@users.noreply.github.com>
2018-12-23 19:33:50 +0300

    Update su-xpos.sh

--- su-xpos.sh
+++ su-xpos.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -v
 apt install git -y
-wget https://dl-xda.xposed.info/modules/com.bigsing.changer_v23_25075d.apk
+
 snap set anbox rootfs-overlay.enable=true
-$ snap restart anbox.container-manager
-sudo chown -R 100000:100000 /var/snap/anbox/common/rootfs-overlay
-git clone
-https://github.com/it-toppp/a-box.git
+snap restart anbox.container-manager
+
+git clone https://github.com/it-toppp/a-box.git
 cp ./a-box/system/ /var/snap/anbox/common/rootfs-overlay
 sudo chown -R 100000:100000 /var/snap/anbox/common/rootfs-overlay
 sudo chmod -R +x /var/snap/anbox/common/rootfs-overlay/system/bin
 sudo chmod -R +x /var/snap/anbox/common/rootfs-overlay/system/xbin
+sudo chmod -R +x /var/snap/anbox/common/rootfs-overlay/system/priv-app/XposedInstaller/XposedInstaller.apk
+
+wget https://dl-xda.xposed.info/modules/com.bigsing.changer_v23_25075d.apk
+adb install ./com.bigsing.changer_v23_25075d.apk
